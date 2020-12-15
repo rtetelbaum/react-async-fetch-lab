65a7ed15302c87a1b1178a52939f62daa81a9015
rtetelbaum <rtetelbaum@mailbox.org>
2020-12-14 22:52:42 -0500

    lab done tests pass

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
-// create your App component here
+import React from 'react'
+
+class App extends React.Component {
+
+	state = {}
+
+	componentDidMount() {
+		fetch('http://api.open-notify.org/astros.json')
+			.then(response => response.json())
+			.then(data => this.setState({people: data.people}))
+	}
+
+	render() {
+		return (
+			{}
+		)
+	}
+}
+
+export default App
